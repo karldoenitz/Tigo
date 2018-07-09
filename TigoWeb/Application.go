5d1dc83ea86bf143c8eb8ef3dd65aacfa4982eae
karl <karlvorndoenitz@gmail.com>
2018-07-09 13:20:49 +0800

    调整了application代码

--- TigoWeb/Application.go
+++ TigoWeb/Application.go
@@ -12,10 +12,11 @@ type Application struct {
 	IPAddress  string      // IP地址
 	Port       string      // 端口
 	UrlPattern UrlPattern  // url路由配置
+	ConfigPath string      // 全局配置
 }
 
 // http服务启动函数
-func (application *Application)Run() {
+func (application *Application)run() {
 	application.UrlPattern.Init()
 	address := fmt.Sprintf("%s:%s", application.IPAddress, application.Port)
 	logger.Info.Printf("Server run on: %s", address)
@@ -25,10 +26,25 @@ func (application *Application)Run() {
 	}
 }
 
-// https服务启动
-func (application *Application)RunTLS(cert string, key string) {
+// https服务启动函数
+func (application *Application)runTLS(cert string, key string) {
 	application.UrlPattern.Init()
 	address := fmt.Sprintf("%s:%s", application.IPAddress, application.Port)
 	logger.Info.Printf("Server run on: %s", address)
 	http.ListenAndServeTLS(address, cert, key, nil)
+}
+
+// 服务启动函数
+func (application *Application)Run() {
+	// 初始化全局变量
+	if application.ConfigPath != "" {
+		InitGlobalConfig(application.ConfigPath)
+	}
+	// 获取证书与密钥，判断是否启动https服务
+	cert, certKey := globalConfig.Cert, globalConfig.CertKey
+	if cert != "" && certKey != "" {
+		application.runTLS(cert, certKey)
+	} else {
+		application.run()
+	}
 }
