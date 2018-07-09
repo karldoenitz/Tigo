53fdd6d81f8ae4fd7b7704eb7cfcbb6dd690669f
karl <karlvorndoenitz@gmail.com>
2018-07-09 10:58:55 +0800

    调整了代码包引入问题

--- TigoWeb/Application.go
+++ TigoWeb/Application.go
@@ -4,17 +4,17 @@ package TigoWeb
 import (
 	"net/http"
 	"fmt"
-	"Tigo/logger"
+	"github.com/Tigo/logger"
 )
 
 // web容器
 type Application struct {
-	IPAddress  string
-	Port       string
-	UrlPattern UrlPattern
+	IPAddress  string      // IP地址
+	Port       string      // 端口
+	UrlPattern UrlPattern  // url路由配置
 }
 
-// 服务启动函数
+// http服务启动函数
 func (application *Application)Run() {
 	application.UrlPattern.Init()
 	address := fmt.Sprintf("%s:%s", application.IPAddress, application.Port)
@@ -23,4 +23,12 @@ func (application *Application)Run() {
 	if httpServerErr != nil {
 		logger.Error.Printf("HTTP SERVER ERROR! MSG: %s", httpServerErr.Error())
 	}
+}
+
+// https服务启动
+func (application *Application)RunTLS(cert string, key string) {
+	application.UrlPattern.Init()
+	address := fmt.Sprintf("%s:%s", application.IPAddress, application.Port)
+	logger.Info.Printf("Server run on: %s", address)
+	http.ListenAndServeTLS(address, cert, key, nil)
 }
