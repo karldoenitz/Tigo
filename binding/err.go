ac41539854ce382933e66d31d5298398605f4abd
lizhihao <karlvorndoenitz@gmail.com>
2018-12-09 13:04:30 +0800

    调整代码

--- binding/err.go
+++ binding/err.go
@@ -1,14 +1,13 @@
 package binding
 
 import (
-	"errors"
 	"fmt"
 )
 
 // NewTagErr 提供了一个错误类型闭包
 func NewTagErr(formatStr string) func(string) error {
 	return func(fieldName string) error {
-		return errors.New(fmt.Sprintf(formatStr, fieldName))
+		return fmt.Errorf(formatStr, fieldName)
 	}
 }
 
--- binding/json.go
+++ binding/json.go
@@ -1,4 +1,4 @@
-// Package binding
+// Package binding 字段校验功能包
 // 提供了一个非常简单的json校验功能，目前功能有限，仅支持几种基本类型的校验。
 package binding
 
@@ -6,7 +6,7 @@ import (
 	"encoding/json"
 )
 
-// ParseJsonToObject 将json转为structure对应的instance，并根据tag校验字段
+// ParseJsonToInstance 将json转为structure对应的instance，并根据tag校验字段
 func ParseJsonToInstance(jsonBytes []byte, obj interface{}) error {
 	err := json.Unmarshal(jsonBytes, obj)
 	if err != nil {
