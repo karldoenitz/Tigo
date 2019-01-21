466ac5f9dad0564cee4b90906dc23dfa15730a85
karllee <karlvorndoenitz@gmail.com>
2019-01-21 14:31:24 +0800

    【操作人】karl 【操作注释】新增代码注释

--- TigoWeb/middleware.go
+++ TigoWeb/middleware.go
@@ -2,8 +2,10 @@ package TigoWeb
 
 import "net/http"
 
+// Middleware http中间件
 type Middleware func(next http.HandlerFunc) http.HandlerFunc
 
+// chainMiddleware 是http中间件生成器
 func chainMiddleware(mw ...Middleware) Middleware {
 	return func(final http.HandlerFunc) http.HandlerFunc {
 		return func(w http.ResponseWriter, r *http.Request) {
