6978f0a7067bb969fe81022e7491bb913ce72e12
karllee <karlvorndoenitz@gmail.com>
2019-11-03 11:29:20 +0800

    bug fix #49

--- TigoWeb/session.go
+++ TigoWeb/session.go
@@ -1,27 +1,27 @@
 package TigoWeb
 
 type SessionInterface interface {
-	// 新建一个SessionManager
+	// NewSessionManager 新建一个SessionManager
 	NewSessionManager() SessionManager
 }
 
 type SessionManager interface {
-	// 生成session
+	// GenerateSession 生成session
 	GenerateSession(expire int) Session
-	// 根据session id获取session
+	// GetSessionBySid 根据session id获取session
 	GetSessionBySid(sid string) Session
-	// 根据session id删除session
+	// DeleteSession 根据session id删除session
 	DeleteSession(sid string)
 }
 
 type Session interface {
-	// 设置session值，设置失败返回error
+	// Set 设置session值，设置失败返回error
 	Set(key string, value interface{}) error
-	// 获取session值，获取失败返回error
+	// Get 获取session值，获取失败返回error
 	Get(key string, value interface{}) error
-	// 删除session
+	// Delete 删除session
 	Delete(key string)
-	// 获取session id
+	// SessionId 获取session id
 	SessionId() string
 }
 
