1fbc814da8547aee7a8f3bb101149c9cce5ecf92
karldoenitz <karlvorndoenitz@gmail.com>
2022-10-13 10:06:43 +0800

    handled warning

--- binding/form.go
+++ binding/form.go
@@ -101,7 +101,7 @@ func getVal(values url.Values, tag string) string {
 
 // bytesToQuery 将bytes转换为url的values值
 func bytesToQuery(urlParam []byte) (url.Values, error) {
-	formatUrl := "http://www.query.com/param?" + string(urlParam)
+	formatUrl := "https://www.query.com/param?" + string(urlParam)
 	u, err := url.Parse(formatUrl)
 	if err != nil {
 		return nil, err
