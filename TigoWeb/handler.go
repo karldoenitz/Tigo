253515d201580242fd4236d4849418c3fb66f387
karldoenitz <karlvorndoenitz@gmail.com>
2022-10-27 10:54:36 +0800

    修改文档

--- TigoWeb/handler.go
+++ TigoWeb/handler.go
@@ -70,6 +70,7 @@ func (baseHandler *BaseHandler) PassJson() {
 
 // ToJson 将对象转化为Json字符串，转换失败则返回空字符串。
 // 传入参数Response为一个interface，必须有成员函数Print。
+//  - response: 需要转换成json的实例
 func (baseHandler *BaseHandler) ToJson(response interface{}) (result []byte) {
 	// 将该对象转换为byte字节数组
 	jsonResult, jsonErr := json.Marshal(response)
