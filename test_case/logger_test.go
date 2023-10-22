03a92ba408d7212aa79f2d6599392c0d684a9d44
karldoenitz <karlvorndoenitz@gmail.com>
2023-10-22 19:28:31 +0800

    增加一个测试用例

--- test_case/logger_test.go
+++ test_case/logger_test.go
@@ -1,1 +1,12 @@
 package test_case
+
+import (
+	"Tigo/logger"
+	"testing"
+)
+
+func TestInitLoggerWithObject(t *testing.T) {
+	logLevel := logger.LogLevel{}
+	logger.InitLoggerWithObject(logLevel)
+	logger.Info.Println("log test passed")
+}
