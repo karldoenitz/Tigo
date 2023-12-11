26da9b686c90bc82cf44c07c819bc20a66879e5b
karldoenitz <karlvorndoenitz@gmail.com>
2023-12-11 12:01:18 +0800

    调整代码目录

--- web/interface.go
+++ web/interface.go
@@ -1,5 +1,5 @@
-// Package TigoWeb Copyright 2018 The Tigo Authors. All rights reserved.
-package TigoWeb
+// Package web Copyright 2018 The Tigo Authors. All rights reserved.
+package web
 
 // Response 响应给客户端的interface，用户自定义实现
 type Response interface {
